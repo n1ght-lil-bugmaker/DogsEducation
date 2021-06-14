5fc855b23807ca7799600baa65d1cab61771274e
n1ght-lil-bugmaker <grachev.alekcandr@mail.ru>
2021-06-14 23:56:46 +0300

    Добавьте файлы проекта.

---
+++ Counter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DogsEducation
+{
+    public class Counter
+    {
+        public event Action<int> HalfReached;
+        public void StartCounting()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if(i == 50)
+                {
+                    HalfReached?.Invoke(i);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DogsEducation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter counter = new Counter();
+
+            counter.HalfReached += OnHalfReached;
+
+            counter.HalfReached += delegate (int c)
+            {
+                Console.WriteLine($"Анонимные делегаты - не очень круто ({c})");
+            };
+
+            counter.HalfReached += c =>
+            {
+                Console.WriteLine($"Лямбды - вот что круто ({c})");
+            };
+ 
+            counter.StartCounting();
+        } // () => {};
+        // x => {};
+        // (x, y, z) => {};
+
+        public static void OnHalfReached(int mya)
+        {
+            Console.WriteLine($"Половина достигнута: {mya}");
+        }
+    }
+}
